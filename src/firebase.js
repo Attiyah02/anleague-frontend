8e42188101432d09780db915785e5e8a0648b16f
Attiyah <4353050@myuwc.ac.za>
2025-11-07 22:20:28 +0200

    Connect frontend to Firebase and update env setup

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,16 @@
+// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBhZSZBjDvA-3TjPvnylpphipiNd7J2v8w",
-  authDomain: "anleague2026.firebaseapp.com",
-  projectId: "anleague2026",
-  storageBucket: "anleague2026.firebasestorage.app",
-  messagingSenderId: "342014639795",
-  appId: "1:342014639795:web:6ee3d9255839b28937bac6",
-  measurementId: "G-2ZPHD4PTC8"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
